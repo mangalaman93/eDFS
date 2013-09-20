661bcc8badad8ce29c1de90bbe9f6abfa57ff567
Aman Mangal <mangalaman93@gmail.com>
2013-09-20 16:34:44 +0530

    initial src file added

---
+++ src/edfsm_metadata_server.erl
@@ -0,0 +1,76 @@
+%% -*- erlang-indent-level: 4;
+%%% Copyright 2012 Erlware, LLC. All Rights Reserved.
+%%%
+%%% This file is provided to you under the Apache License,
+%%% Version 2.0 (the "License"); you may not use this file
+%%% except in compliance with the License.  You may obtain
+%%% a copy of the License at
+%%%
+%%%   http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing,
+%%% software distributed under the License is distributed on an
+%%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%%% KIND, either express or implied.  See the License for the
+%%% specific language governing permissions and limitations
+%%% under the License.
+%%% --------------------------------------------------------------------------
+%%% @author Aman Mangal <mangalaman93@gmail.com>
+%%% @doc edfs metadata server on master node
+%%%
+
+-module(edfsm_metadata_server).
+-behaviour(gen_server).
+-include("edfs.hrl").
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([start_link/0]).
+
+%% start_link/0
+%% ====================================================================
+%% @doc starts the metadata server on master node
+-spec start_link() -> Result when
+    Result :: {error, Reason :: term()}
+            | {ok, Pid :: pid()}.
+%% ====================================================================
+start_link() ->
+    gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
+
+
+%% ====================================================================
+%% Behavioural functions
+%% ====================================================================
+
+%% @private
+init([]) ->
+    {ok, {}}.
+
+%% @private
+handle_call(_Request, _From, State) ->
+    {reply, {error, unknown_call}, State}.
+
+%% @private
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%% @private
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% @private
+terminate(_Reason, _State) ->
+    ok.
+
+%% @private
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
